3f7da197f108f507c290171205d03467cf5a89c4
Zeev <zeev.lieber@gmail.com>
2022-12-21 16:17:24 -0500

    Real heat controller and temp monitor

--- oven/heat_controller.py
+++ oven/heat_controller.py
@@ -4,17 +4,22 @@ from datetime import datetime
 class HeatController:
     def __init__(self):
         self.is_on = False
+        self.dev = "/sys/devices/platform/pinctrl/gpio/gpio71/value"
 
     def heat_on(self):
         if self.is_on:
             return
         print("Heat ON")
+        with open(self.dev, "w") as f:
+            f.write("0")
         self.is_on = True
 
     def heat_off(self):
         if not self.is_on:
             return
         print("Heat OFF")
+        with open(self.dev, "w") as f:
+            f.write("1")
         self.is_on = False
 
     def on_temp_change(self):
--- oven/temp_monitor.py
+++ oven/temp_monitor.py
@@ -1,8 +1,12 @@
 
+import mraa
+
 class TemperatureMonitor:
     def __init__(self):
-        pass
+        self.dev = mraa.Spi(0)
+        self.dev.frequency(1000000)
 
     def get_temperature(self):
-        # SPI stuff goes here
-        pass
+        raw = self.dev.writeWord(1)
+        # Bits 0, 1, 2 and 15 are 0s.
+        return float((( (raw & 0x7F) << 8) | raw >> 8) >> 3) / 4.0
